4152f14e96bf496cb9b871878724faba6d8de7d3
limseungkyu <corla@fastlane.kr>
2022-09-02 23:16:51 +0900

    Google Book 응답 모델 추가

---
+++ app/src/main/java/com/bookfinder/model/Book.kt
@@ -0,0 +1,88 @@
+package com.bookfinder.model
+
+object Book {
+    class RS(var kind: String, var totalItems: String, var items: ArrayList<Items>) {
+        class Items(
+            var kind: String,
+            var id: String,
+            var etag: String,
+            var selfLink: String,
+            var volumeInfo: VolumeInfo,
+            var saleInfo: SaleInfo,
+            var accessInfo: AccessInfo,
+            var searchInfo: SearchInfo
+        ) {
+            class VolumeInfo(
+                var title: String,
+                var subtitle: String,
+                var authors: ArrayList<String>,
+                var publisher: String,
+                var publishedDate: String,
+                var description: String,
+                var industryIdentifiers: ArrayList<IndustryIdentifiers>,
+                var readingModes: ReadingModes,
+                var pageCount: Int,
+                var printType: String,
+                var categories: ArrayList<String>,
+                var averageRating: Int,
+                var ratingsCount: Int,
+                var maturityRating: String,
+                var allowAnonLogging: Boolean,
+                var contentVersion: String,
+                var panelizationSummary: PanelizationSummary,
+                var imageLinks: ImageLinks,
+                var language: String,
+                var previewLink: String,
+                var infoLink: String,
+                var canonicalVolumeLink: String
+            ) {
+                class IndustryIdentifiers(var type: String, var identifier: String)
+                class ReadingModes(var text: Boolean, var image: Boolean)
+                class PanelizationSummary(var text: Boolean, var image: Boolean)
+                class ImageLinks(var smallThumbnail: String, var thumbnail: String)
+            }
+
+            class SaleInfo(
+                var country: String,
+                var saleability: String,
+                var isEbook: Boolean,
+                var listPrice: Price,
+                var retailPrice: Price,
+                var buyLink: String,
+                var offers: Array<Offer>
+            ) {
+                class Price(var amount: Int, var currencyCode: String)
+                class Offer(
+                    var finskyOfferType: String,
+                    var currencyCode: String,
+                    var listPrice: MicrosPrice,
+                    var retailPrice: MicrosPrice
+                ){
+                    class MicrosPrice(var amountInMicros: Long, var currencyCode: String)
+                }
+            }
+
+            class AccessInfo(
+                var country: String,
+                var viewability: String,
+                var embeddable: Boolean,
+                var publicDomain: Boolean,
+                var textToSpeechPermission: String,
+                var epub: Epub,
+                var pdf: Pdf,
+                var webReaderLink: String,
+                var accessViewStatus: String,
+                var quoteSharingAllowed: Boolean
+            ) {
+                class Epub(var isAvailable: Boolean, var acsTokenLink: String)
+                class Pdf(var isAvailable: Boolean)
+            }
+
+            class SearchInfo(var textSnippet: String)
+        }
+    }
+}
+
+
+
+
