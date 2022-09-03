646acdba3835d542cd855ef1169a57f9dab2f54f
임승규 <sklim0221@gmail.com>
2022-09-03 16:38:31 +0900

    뷰모델을 통해 api 호출 추가

--- app/src/main/java/com/bookfinder/MainViewModel.kt
+++ app/src/main/java/com/bookfinder/MainViewModel.kt
@@ -1,10 +1,9 @@
 package com.bookfinder
 
 import android.util.Log
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
 import com.bookfinder.constants.Constants
+import com.bookfinder.model.Book
 import com.bookfinder.repository.BookRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -14,12 +13,22 @@ class MainViewModel @Inject constructor(
     private val repository: BookRepository,
     private val savedStateHandle: SavedStateHandle
 ) : ViewModel(), DefaultLifecycleObserver {
+    private val books: MutableLiveData<Book.RS> by lazy {
+        MutableLiveData<Book.RS>().also {
+            laodBooks()
+        }
+    }
 
-    fun getBookList() {
+    private fun laodBooks() {
         repository.getBookList(Constants.GOOGLE_KEY, {
-            Log.i("book", "$it")
+            var total = it.totalItems
+            books.value = it
         }, {
             Log.i("book", "${it.message}")
         })
+    }
+
+    fun getBookList(): LiveData<Book.RS> {
+        return books
     }
 }
