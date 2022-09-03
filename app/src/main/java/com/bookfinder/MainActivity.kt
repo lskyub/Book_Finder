6af20284d7cb5b4b45d3fe80ad0747dad70eb20f
임승규 <sklim0221@gmail.com>
2022-09-03 20:28:51 +0900

    스크롤 이벤트 및 검색 로직 추가

--- app/src/main/java/com/bookfinder/MainActivity.kt
+++ app/src/main/java/com/bookfinder/MainActivity.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bookfinder.custom.BookAdapter
 import com.bookfinder.custom.BookDecoration
-import com.bookfinder.custom.BookScrollListener
 import com.bookfinder.model.Book
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,32 +27,32 @@ class MainActivity : AppCompatActivity() {
         val rvBooks = findViewById<RecyclerView>(R.id.rv_books).apply {
             adapter = BookAdapter(context, differ)
             addItemDecoration(BookDecoration(20))
-            addOnScrollListener(object : BookScrollListener(layoutManager as LinearLayoutManager) {
-                override fun onLoadMore() {
-                    val listCount = (adapter as BookAdapter).itemCount
-                    viewModel.getNextBookList(listCount)
-                }
-            })
+            addOnScrollListener(viewModel.getScrollListener(layoutManager as LinearLayoutManager))
         }
 
+        findViewById<AppCompatEditText>(R.id.et_book).apply {
+            addTextChangedListener(viewModel.textWatcher)
+        }
+
+        findViewById<AppCompatImageView>(R.id.btn_search).setOnClickListener(viewModel)
+
+        viewModel.getTotal().observe(this) {
+            results.text = "Results : $it"
+
+        }
         viewModel.getBookList().observe(this) {
-            results.text = "Results : ${it.totalItems}"
-            (rvBooks.adapter as BookAdapter)?.run {
-                if (this.itemCount != 0) {
-                    submitList(arrayListOf<Book.RS.Items>().apply {
-                        addAll(currentList)
-                        addAll(it.items)
-                    })
-                } else {
-                    submitList(it.items)
-                }
+            (rvBooks.adapter as BookAdapter).run {
+                submitList(it.toMutableList())
             }
         }
     }
 
     private val differ: DiffUtil.ItemCallback<Book.RS.Items> =
         object : DiffUtil.ItemCallback<Book.RS.Items>() {
-            override fun areItemsTheSame(oldItem: Book.RS.Items, newItem: Book.RS.Items): Boolean {
+            override fun areItemsTheSame(
+                oldItem: Book.RS.Items,
+                newItem: Book.RS.Items
+            ): Boolean {
                 return oldItem == newItem
             }
 
--- app/src/main/java/com/bookfinder/MainViewModel.kt
+++ app/src/main/java/com/bookfinder/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.bookfinder
 
 import android.util.Log
+import android.view.View
 import androidx.lifecycle.*
-import com.bookfinder.constants.Constants
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bookfinder.custom.BookScrollListener
+import com.bookfinder.custom.SearchTextWatcher
 import com.bookfinder.model.Book
 import com.bookfinder.repository.BookRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,29 +15,60 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val repository: BookRepository,
     private val savedStateHandle: SavedStateHandle
-) : ViewModel(), DefaultLifecycleObserver {
+) : ViewModel(), DefaultLifecycleObserver, View.OnClickListener {
     private val mMaxResult = 20
 
-    private val books: MutableLiveData<Book.RS> by lazy {
-        MutableLiveData<Book.RS>().also {
-            laodBooks(0)
+    private var beforStr = ""
+
+    private val booksList = arrayListOf<Book.RS.Items>()
+
+    private val books: MutableLiveData<List<Book.RS.Items>> = MutableLiveData<List<Book.RS.Items>>()
+
+    private val total: MutableLiveData<String> = MutableLiveData<String>()
+
+    private fun laodBooks(str: String = beforStr, start: Int = booksList.size) {
+        if (str.isNullOrEmpty() || str.isNullOrBlank()) {
+            total.value = ""
+            books.value = booksList
+        } else {
+            repository.getBookList("$str", mMaxResult, start, {
+                booksList.addAll(it.items)
+                books.value = booksList
+                total.value = it.totalItems
+            }, {
+                Log.i("book", "${it.message}")
+            })
         }
+
     }
 
-    private fun laodBooks(startIndex: Int) {
-        repository.getBookList("flowers+inauthor", mMaxResult, startIndex, {
-            books.value = it
-        }, {
-            Log.i("book", "${it.message}")
-        })
+    fun getTotal(): LiveData<String> {
+        return total
     }
 
-    fun getBookList(): LiveData<Book.RS> {
+    fun getBookList(): LiveData<List<Book.RS.Items>> {
         return books
     }
 
-    fun getNextBookList(startIndex: Int) {
-        Log.i("sgim", "${startIndex}")
-        laodBooks(startIndex)
+    fun getScrollListener(layoutManager: LinearLayoutManager): BookScrollListener {
+        return object : BookScrollListener(layoutManager) {
+            override fun onLoadMore() {
+                laodBooks()
+            }
+        }
+    }
+
+    val textWatcher = object : SearchTextWatcher() {
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            beforStr = s?.toString() ?: ""
+            booksList.clear()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btn_search) {
+            booksList.clear()
+            laodBooks()
+        }
     }
 }
