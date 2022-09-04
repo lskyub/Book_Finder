95f6fa58d2023bc3ed798f36e4ab000b4f025861
임승규 <sklim0221@gmail.com>
2022-09-04 17:39:42 +0900

    api를 취소하는 함수 추가

--- app/src/main/java/com/bookfinder/repository/BookRepository.kt
+++ app/src/main/java/com/bookfinder/repository/BookRepository.kt
@@ -1,20 +1,22 @@
 package com.bookfinder.repository
 
 import android.content.Context
-import android.util.Log
 import com.bookfinder.model.Book
 import com.bookfinder.network.ApiService
 import dagger.hilt.android.qualifiers.ApplicationContext
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
+
 
 class BookRepository @Inject constructor(
     @ApplicationContext
     val applicationContext: Context,
     private val api: ApiService
 ) {
+    var compositeDisposable = CompositeDisposable()
     fun getBookList(
         str: String,
         maxResult: Int,
@@ -22,7 +24,13 @@ class BookRepository @Inject constructor(
         result: Consumer<Book.RS>,
         error: Consumer<Throwable>
     ) {
-        api.search(str, maxResult, startIndex).subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread()).subscribe(result, error)
+        compositeDisposable.add(
+            api.search(str, maxResult, startIndex).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(result, error)
+        )
+    }
+
+    fun cancelApi() {
+        compositeDisposable.clear()
     }
 }
